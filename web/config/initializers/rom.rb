b589d9390616d7ada657528d9b6bf38a71a615fc
Matt Bessey <mbessey@caring.com>
2018-07-15 17:50:33 +0100

    delete rails

--- web/config/initializers/rom.rb
+++
@@ -1,4 +0,0 @@
-ROM::Rails::Railtie.configure do |config|
-  credentials = Web::Application.credentials.database
-  config.gateways[:default] = [:sql, credentials]
-end
--- web/db/migrate/20180715142411_create_user.rb
+++
@@ -1,8 +0,0 @@
-ROM::SQL.migration do
-  change do
-    create_table :users do
-      primary_key :id
-      column :email, String, null: false
-    end
-  end
-end
